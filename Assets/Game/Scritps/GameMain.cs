cd5a10c2fa7a33d9f9109d7baf5a0e0f4b204f6b
Star <starlove124760@gmail.com>
2018-01-27 17:48:56 +0800

    亂數選出一位。

--- Assets/Game/Scritps/GameMain.cs
+++ Assets/Game/Scritps/GameMain.cs
@@ -1,20 +1,30 @@
 ﻿using UnityEngine;
-using System.Collections;
 using Zenject;
+using UniRx;
+
 public class GameMain : MonoBehaviour
 {
-    [Inject]
-    private ObjectSpwner objectSpwner;
-
+    [SerializeField] private ObjectSpwner[] objectSpwners;
+    [Inject] private SpawnManager spawnManager;
+    
     // Use this for initialization
     void Start()
     {
+        objectSpwners = FindObjectsOfType<ObjectSpwner>();
+        for (var i = 0; i < objectSpwners.Length; i++)
+        {
+            objectSpwners[i].Spawn();
+        }
 
+        //InitEnd();
+        Observable.EveryUpdate()
+            .Where(x => Input.GetKeyDown(KeyCode.A))
+            .Subscribe(_ => InitEnd());
     }
 
-    // Update is called once per frame
-    void Update()
+    public void InitEnd()
     {
-
+        spawnManager.GetRandomNotRepeatingObj(true);
     }
+
 }
--- Assets/Game/Scritps/ObjectSpwner.cs
+++ Assets/Game/Scritps/ObjectSpwner.cs
@@ -1,7 +1,45 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿
+using System;
 using UnityEngine;
+using Zenject;
 
-public class ObjectSpwner  {
+public class ObjectSpwner : MonoBehaviour
+{
+    [SerializeField] private Transform firstTransform;
 
+    [Header("行")] [SerializeField] private int row;
+    [Header("列")] [SerializeField] private int column;
+    [Space(10)] [SerializeField] private float rowOffset, columnOffset;
+    [Inject] private ClickManager clickManager;
+    [Inject] private SpawnManager spawnManager;
+    [Inject] private GameMain gameMain;
+    private static int spawnIndex = 1;
+
+
+    public void Spawn()
+    {
+        firstTransform = transform.GetChild(0);
+        Transform parentTransform = firstTransform.parent;
+        Vector3 firstPostion = firstTransform.position;
+        string initName = firstTransform.name;
+
+        for (int i = 0; i < row; i++)
+        {
+            for (int j = 0; j < column; j++)
+            {
+                string newName = String.Format("{0} [{1}]", initName, spawnIndex);
+
+                Vector3 xOffset = Vector3.right * rowOffset * i;
+                Vector3 yOffset = Vector3.down * columnOffset * j;
+                Vector3 spawnPos = firstPostion + xOffset + yOffset;
+                GameObject newObj = Instantiate(firstTransform, spawnPos
+                    , Quaternion.identity, parentTransform).gameObject;
+                newObj.name = newName;
+                spawnIndex++;
+                clickManager.SubscribeMouseDown(newObj);
+                spawnManager.PutIntoPool(newObj);
+            }
+        }
+        firstTransform.gameObject.SetActive(false);
+    }
 }
